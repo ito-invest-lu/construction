f411d68948deb5d08570ea3ead1652e1c97fd376
Jerome Sonnet (be-cloud.be) <jerome.sonnet@imply.lu>
2017-11-21 20:02:15 +0000

    add default agreement

--- construction_reduced_vat/models/construction_reduced_vat.py
+++ construction_reduced_vat/models/construction_reduced_vat.py
@@ -97,18 +97,18 @@ class ReducedVATAgreement(models.Model):
         used_amount = sum(invoice.amount_untaxed for invoice in self.invoice_ids)
         self.agreement_remaining_amount = self.agreement_total_amount - used_amount
 
-# class SaleOrder(models.Model):
-#     '''Sale Order'''
-#     _inherit = "sale.order"
+class SaleOrder(models.Model):
+    '''Sale Order'''
+    _inherit = "sale.order"
     
-#     @api.multi
-#     def _prepare_invoice(self):
-#         invoice_vals = super(SaleOrder, self)._prepare_invoice()
-        
-#         self.env['']
-#         invoice_vals['building_asset_id'] = self.building_asset_id.id or False
-        
-#         return invoice_vals
+    @api.multi
+    def _prepare_invoice(self):
+        self.ensure_one()
+        invoice_vals = super(SaleOrder, self)._prepare_invoice()
+        agreement_ids = self.env['construction.reduced_vat_agreement'].search([('partner_id', '=', self.partner_id),('agreement_remaining_amount','>',0)])
+        if agreement_ids:
+            invoice_vals['reduced_vat_agreement_id'] = agreement_ids[0].id
+        return invoice_vals
     
 class AccountInvoice(models.Model):
     '''Invoice'''
