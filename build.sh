d438e3dd2b00d44a8fc37fca088522ed76a23f9d
Jerome Sonnet (be-cloud) <jerome.sonnet@be-cloud.be>
2018-01-10 22:32:56 +0000

    add build.sh

---
+++ build.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+# delete all __init__.py files as they are not required since Odoo 11.0
+find . -name '__init__.py' | xargs rm -rf
+
+cd setup
+
+for f in ./*; do
+    echo 'Deploy module' $f
+    if [ -d $f ]; then
+        cd $f
+        echo 'Deploy module' $f
+        python3 setup.py sdist upload -r pypi-dev
+        cd ..
+    fi
+done
