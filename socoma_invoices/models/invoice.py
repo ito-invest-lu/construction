fcbb85112cf16f0925d7fdc946ee082dc6460525
Jerome Sonnet (be-cloud.be) <jerome.sonnet@imply.lu>
2018-10-22 09:41:40 +0000

    add comuted tax on invoice

---
+++ socoma_invoices/models/invoice.py
@@ -0,0 +1,43 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (c) 2015 be-cloud.be
+#                       Jerome Sonnet <jerome.sonnet@be-cloud.be>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import logging
+
+from openerp import api, fields, models, _
+from openerp.exceptions import UserError, ValidationError
+
+_logger = logging.getLogger(__name__)
+
+class Invoice(models.Model):
+    '''Invoice'''
+    _inherit = 'account.invoice'
+    
+    first_line_tax_id = fields.Many2one('account.tax.template', string='Fist Line Tax', compute='_compute_first_line_tax_id')
+    
+    @api.multi
+    def _compute_first_line_tax_id(self):
+        for invoice in self:
+            tax_id = False
+            if len(invoice.invoice_line_ids) > 0 :
+                line = invoice.invoice_line_ids[0]
+                if len(line.invoice_line_tax_ids) > 0 :
+                    tax_id = line.invoice_line_tax_ids[0]
+            invoice.first_line_tax_id = tax_id
+        
