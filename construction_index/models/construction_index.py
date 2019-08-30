2bb8e50a1d6be9c6d324708a2c0cdcdfdf61258d
be-cloud-js <jerome.sonnet@be-cloud.be>
2019-08-30 07:49:03 +0200

    first draft

--- construction_index/models/construction_index.py
+++ construction_index/models/construction_index.py
@@ -36,11 +36,12 @@ class SaleOrder(models.Model):
     initial_index = fields.Float(string="Initial Index", readonly=True, states={'draft': [('readonly', False)]})
     current_index = fields.Float(string="Current Index", readonly=False, states={'draft': [('readonly', True)]})
 
-    @api.depends('initial_index')
-    def _setup_initial_index(self):
-        _logger.info(self)
-        if self.state == 'draft' :
-            self.current_index = self.initial_index
+    @api.multi
+     def write(self, vals):
+         if state == 'draft' and 'initial_index' in vals :
+             _logger.info('Change current_index')
+             vals['current_index'] = vals['initial_index']
+         return super(SaleOrder, self).write(vals)
 
 class SaleOrderLine(models.Model):
     '''Sale Order Line'''
