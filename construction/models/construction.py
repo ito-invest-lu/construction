5d859b71219ecf2499d80f4f104157a368b6bded
Jérôme Sonnet <jerome.sonnet@imply.lu>
2020-03-25 16:20:18 +0100

    add analytic account ref

--- construction/models/construction.py
+++ construction/models/construction.py
@@ -72,6 +72,8 @@ class BuildingAsset(models.Model):
     confirmed_lead_id = fields.Many2one('crm.lead', string='Confirmed Lead')
 
     candidate_lead_ids = fields.One2many('crm.lead', 'building_asset_id', string='Candidate Leads', domain=['|',('active','=',True),('active','=',False)])
+    
+    analytic_account = fields.text(string="BookIn Analytic")
 
     @api.onchange('confirmed_lead_id')
     def update_confirmed_lead_id(self):
@@ -172,6 +174,8 @@ class SaleOrderTag(models.Model):
     name = fields.Char(string='Analytic Tag', index=True, required=True)
     color = fields.Integer('Color Index')
     active = fields.Boolean(default=True, help="Set active to false to hide the Sale Order Tag without removing it.")
+
+    analytic_account = fields.text(string="BookIn Analytic")
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
