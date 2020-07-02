a75006957ee69e70fb8c96a4f4f8f4285f33bc01
Jérôme Sonnet <jerome.sonnet@imply.lu>
2020-07-02 08:46:43 +0200

    add order sign module

---
+++ construction_sign_sale_order/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+# see LICENSE file
+
+{
+    'name': 'Construction Sign Sale Order',
+    'version': '0.1',
+    'category': 'Sales',
+    'description': """
+    Sign sale orders.
+    """,
+    "author": "be-cloud.be (Jerome Sonnet)",
+    "website": "http://www.be-cloud.be",
+    'depends': ['construction'],
+    'init_xml': [],
+    'data': [
+        'views/construction_sign_sale_order.xml',
+    ],
+    'installable': True,
+    'active': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
