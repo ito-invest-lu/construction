0645374761da7433f672c83c9c7eeea79d66d4cd
Jérôme Sonnet <jerome.sonnet@imply.lu>
2020-07-02 08:43:55 +0200

    add order sign module

---
+++ construction_sign_sale_order/models/construction_sign_sale_order.py
@@ -0,0 +1,35 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (c) 2015 be-cloud.be
+#                       Jerome Sonnet <jerome.sonnet@be-cloud.be>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import logging
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
+
+import odoo.addons.decimal_precision as dp
+
+_logger = logging.getLogger(__name__)
+
+class SaleOrder(models.Model):
+    '''Sale Order'''
+    _inherit = "sale.order"
+
+    upload_order_details = fields.Binary(string="Upload Order Details")
+    order_details_file_name = fields.Char(string="Order Details File Name")
