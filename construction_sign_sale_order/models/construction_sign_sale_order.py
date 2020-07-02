6444488f36be67079b7fac782b67d011eff36af0
Jérôme Sonnet <jerome.sonnet@imply.lu>
2020-07-02 16:27:42 +0200

    add order sign module

--- construction_sign_sale_order/models/construction_sign_sale_order.py
+++ construction_sign_sale_order/models/construction_sign_sale_order.py
@@ -27,9 +27,55 @@ import odoo.addons.decimal_precision as dp
 
 _logger = logging.getLogger(__name__)
 
+class SignRequest(models.Model):
+    _name = 'sign.request'
+    _inherit = ['sign.request', 'documents.mixin']
+
+    sale_order_id = fields.Many2one(related='template_id.sale_order_id', readonly=True)
+
+class SignTemplate(models.Model):
+    _name = 'sign.template'
+    _inherit = ['sign.template', 'documents.mixin']
+
+    sale_order_id = fields.Many2one('sale.order', 'Signed Sale Order')
+
 class SaleOrder(models.Model):
     '''Sale Order'''
     _inherit = "sale.order"
 
-    upload_order_details = fields.Binary(string="Upload Order Details")
+    upload_order_details = fields.Binary(string="Upload Order Details", attachment=True)
     order_details_file_name = fields.Char(string="Order Details File Name")
+    
+    order_details_sign_request_ids = fields.One2many('sign.request', 'sale_order_id', string="Sign requests")
+    order_details_sign_request_count = fields.Integer(compute='_compute_sign_request_count', string='Sign requests Count')
+    
+    @api.depends('order_details_sign_request_ids')
+    def _compute_mailing_mail_count(self):
+        for so in self:
+            so.order_details_sign_request_count = len(so.order_details_sign_request_ids)
+    
+    @api.multi
+    def sign_sale_order_details(self):
+        
+        if not upload_order_details :
+            raise UserError('Vous devez d''abords uploader le détail du devis')
+        
+        create_values = {
+            'name': this.name + '-' + this.partner_id.name,
+            'attachment_id': self.upload_order_details.id,
+            'favorited_ids': [(4, self.env.user.id)],
+        }
+
+        new_obj = self.env['sign.template'].create(create_values)
+        
+        action = {
+            'type': 'ir.actions.act_window',
+            'res_model': 'sign.template',
+            'name': _("New templates"),
+            'view_id': False,
+            'view_mode': 'form',
+            'views': [(False, "kanban"), (False, "form")],
+            'domain': [('id', '=', new_obj.id)],
+            'context': self._context,
+        }
+        
