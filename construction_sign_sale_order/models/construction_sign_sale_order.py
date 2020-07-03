7ebc3932155086bfef1295816de08c9e187f7aa2
Jérôme Sonnet <jerome.sonnet@imply.lu>
2020-07-03 12:25:13 +0200

    add order sign module

--- construction_sign_sale_order/models/construction_sign_sale_order.py
+++ construction_sign_sale_order/models/construction_sign_sale_order.py
@@ -86,11 +86,13 @@ class SaleOrder(models.Model):
             'required'  : True,
         }]
 
+        new_items = self.env['signature.item'].create(create_signature_items_value)
+
         create_values = {
             'attachment_id': attachment[0].id,
             'favorited_ids': [(4, self.env.user.id)],
             'sale_order_id' : self.id,
-            'signature_item_ids' : [(0,0,create_signature_items_value)],
+            'signature_item_ids' : [(0,0,new_items.ids)],
         }
 
         new_obj = self.env['signature.request.template'].create(create_values)
