a0cefb905ef358f3decbfb45751897db579506a7
Jérôme Sonnet <jerome.sonnet@imply.lu>
2021-06-10 13:12:15 +0200

    fix

--- construction_extranet/controllers/main.py
+++ construction_extranet/controllers/main.py
@@ -39,8 +39,8 @@ class ConstructionController(http.Controller):
     def invoices(self, company_id, debug=False, **k):
         values = {
             'company_id': request.env['res.company'].browse(company_id),
-            'draft_invoice_ids' : request.env['account.move'].sudo().search([('company_id','=',company_id),('state','=','draft'),('type', '=', 'out_invoice')]),
-            'open_invoice_ids' : request.env['account.move'].sudo().search([('company_id','=',company_id),('state','=','posted'),('amount_residual','!=','0'),('type', '=', 'out_invoice')]),
-            'paid_invoice_ids' : request.env['account.move'].sudo().search([('company_id','=',company_id),('state','=','posted'),('amount_residual','=','0'),('type', '=', 'out_invoice')]),
+            'draft_invoice_ids' : request.env['account.move'].sudo().search([('company_id','=',company_id),('state','=','draft'),('type', '=', 'in_invoice')]),
+            'open_invoice_ids' : request.env['account.move'].sudo().search([('company_id','=',company_id),('state','=','posted'),('amount_residual','!=','0'),('type', '=', 'in_invoice')]),
+            'paid_invoice_ids' : request.env['account.move'].sudo().search([('company_id','=',company_id),('state','=','posted'),('amount_residual','=','0'),('type', '=', 'in_invoice')]),
         }
         return request.render('construction_extranet.invoices', values)
