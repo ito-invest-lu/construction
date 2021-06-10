7e9f89f432339e366f3584307ffe31979d6d9af9
Jérôme Sonnet <jerome.sonnet@imply.lu>
2021-06-10 13:42:15 +0200

    fix

--- construction_extranet/controllers/main.py
+++ construction_extranet/controllers/main.py
@@ -58,6 +58,6 @@ class ConstructionController(http.Controller):
         
     @http.route('/invoice_original/<int:invoice_id>', type='http', auth='none', csrf=False)
     def invoices_cust(self, invoice_id, debug=False, **k):
-        invoice = request.env['account.move'].browse(invoice_id), 
+        invoice = request.env['account.move'].sudo().browse(invoice_id), 
         if invoice :
             return werkzeug.utils.redirect('/web/content/%s' % invoice[0].message_main_attachment_id.id)
